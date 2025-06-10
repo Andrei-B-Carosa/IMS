27a56ac7983706f61864b3d9bef166a6a6fab959
rvl-mis-andrei <andrei.carosa@rvlmovers.com>
2025-06-10 15:59:15 +0800

    updated config

--- config/company.php
+++ config/company.php
@@ -4,6 +4,6 @@ return [
     'project_name'=>'Issuance Management System',
     'company_address'=>'Blk 2 Lot 2 Filinvest Technology Park, Ciudad de Calamba Rd, Calamba, Laguna',
     'company_email'=>'exelpack@corporate.com',
-    'company_domain'=>'corporate_domain.com',
+    'company_domain'=>'rvlmovers.com',
     'item_code'=>'RVL',
 ];
