d8b145d3c4c139e86cc7f14ab3b4988f7b29563b
rvl-mis-andrei <andrei.carosa@rvlmovers.com>
2025-06-25 10:35:46 +0800

    update hrispost

--- database/seeders/HrisPositionSeeder.php
+++ database/seeders/HrisPositionSeeder.php
@@ -719,6 +719,24 @@ class HrisPositionSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+            [
+                'name' => 'Property Administrator.',
+                'description' => 'Property Administrator.',
+                'is_active' => 1,
+                'created_by' => 1,
+                'updated_by' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Maintenance Helper.',
+                'description' => 'Maintenance Helper.',
+                'is_active' => 1,
+                'created_by' => 1,
+                'updated_by' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
