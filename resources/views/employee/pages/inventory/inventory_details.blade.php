2572dcf71ba3363f558ee4d2f71b04e24cfab166
rvl-mis-andrei <andrei.carosa@rvlmovers.com>
2025-06-25 10:10:33 +0800

    update inventory-details

--- resources/views/employee/pages/inventory/inventory_details.blade.php
+++ resources/views/employee/pages/inventory/inventory_details.blade.php
@@ -59,12 +59,10 @@
                         </select>
                         <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
                     </div>
-                    @if($data->item_type_id == 1 || $data->item_type_id ==8)
-                        <div class="mb-7 fv-row">
-                            <label class="required form-label">Tag Number</label>
-                            <input type="text" name="tag_number" class="form-control mb-2" placeholder="Item Name" value="{{ $data->generate_tag_number() }}" disabled/>
-                        </div>
-                    @endif
+                    <div class="mb-7 fv-row">
+                        <label class="required form-label">Tag Number</label>
+                        <input type="text" name="tag_number" class="form-control mb-2" placeholder="Item Name" value="{{ $data->generate_tag_number() }}" disabled/>
+                    </div>
                     @if($data->item_type_id != 1 && $data->item_type_id !=8)
                         <div class="fv-row mb-7 fv-plugins-icon-container" id="">
                             <label class="fs-6 fw-semibold mb-2">Description</label>
