b3dcbf926fbd06426003606a77d6cce19598c07c
rvl-mis-andrei <andrei.carosa@rvlmovers.com>
2025-07-28 13:52:06 +0800

    update dashboard and file

--- app/Service/Widget/RouterCount.php
+++ app/Service/Widget/RouterCount.php
@@ -18,7 +18,8 @@ class RouterCount
     public function show()
     {
         $router_id = ImsItemType::getItemTypeId('router');
-        $count = ImsItemInventory::where([['item_type_id',$router_id],['is_deleted',null],['status','!=',0]])->count();
+        $modem_id = ImsItemType::getItemTypeId('modem');
+        $count = ImsItemInventory::whereIn('item_type_id',[$router_id,$modem_id])->where([['is_deleted',null],['status','!=',0]])->count();
         return response()->json(['status' => 'success', 'message'=>'success', 'payload'=>$count]);
     }
 }
